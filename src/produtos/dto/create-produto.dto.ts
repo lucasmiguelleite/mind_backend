16b8057a628ac6a9b28251fe4026a759fbd10834
Lucas <lucasleite.miguel10@gmail.com>
2025-03-06 13:34:18 -0300

    feat: api dos produtos criada

---
+++ src/produtos/dto/create-produto.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty } from "class-validator";
+
+export class CreateProdutoDto {
+ @IsNotEmpty()
+  nome: string;
+
+  @IsNotEmpty()
+  descricao: string;
+
+  @IsNotEmpty()
+  valor: number;
+
+  @IsNotEmpty()
+  estoque: number;
+
+  @IsNotEmpty()
+  imagem: string; 
+}
